a33102a6dfe1faece526f6bcfbce86a2cc04580b
Shantia Grimes-Farmer <shantiagrimes@gmail.com>
2018-06-08 16:13:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  let number = katzDeliLine.indexOf(name) + 2;
+  katzDeliLine.push(name);
+  let number = katzDeliLine.indexOf(name) + 1;
   return `Welcome, ${name}. You are number ${number} in line.`;
 }
