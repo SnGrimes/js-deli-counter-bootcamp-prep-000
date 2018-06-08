3e1ec386abf2309dad478b0dbdd9261f6380eea3
Shantia Grimes-Farmer <shantiagrimes@gmail.com>
2018-06-08 16:56:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function nowServing(katzDeli) {
     return `Currently serving ${name}.`;
   }
 }
+
+function currentLine(line) {
+  if(line.length === 0) {
+    return "The line is currently empty.";
+  }
+  else {
+    
+  }
+}
